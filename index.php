aa4c82f4af712ff0d22b42ecf96346a8014aa5f9
decolevandro78-art <decolevandro78@gmail.com>
2026-06-23 22:40:14 -0300

    tive que fazer uma estrutura html

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-oii ;3
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resinoir</title>
+</head>
+<body>
+    oii ;3
+</body>
+</html>
+
