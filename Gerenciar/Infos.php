36cf5284bd7351d706a6732f8e571cc01656d0fa
decolevandro78-art <noirresina73@gmail.com>
2026-07-01 23:34:56 -0300

    adicione o sitema de mudar de card pra infos

--- Gerenciar/Infos.php
+++ Gerenciar/Infos.php
@@ -60,7 +60,7 @@ include 'autoloader.php';
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text" id="inputGroup-sizing-default">Capa</span>
-            <input required="true" id="inputCapa" oninput="updatePost();" name="capa" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            <input required="true" id="inputCapa" accept="image/*" onchange="handleImageUpload(event)" name="capa" type="file" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text">R$</span>
@@ -89,18 +89,18 @@ include 'autoloader.php';
       </form>
       </div>
 
-      <div class="postagem">
+      <div class="postagem fixed" id="postagem">
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-          <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
+          <input type="radio" class="btn-check" name="btnradio" oninput="noneCarrocelUpdate();" id="btnradio1" autocomplete="off" checked>
+          <label class="btn btn-outline-primary" for="btnradio1">Card</label>
           
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-          <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
+          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" oninput="noneCardUpdate();" autocomplete="off">
+          <label class="btn btn-outline-primary" for="btnradio3">Informaçoes</label>
         </div>
 
 
 
-        <div class="post">
+        <div class="post" id="post">
         <div class="card" style="width: 18rem;" data-bs-theme="dark">
           <img id="cardCapa" src="" class="card-img-top" alt="...">
           <div class="card-body">
@@ -109,8 +109,47 @@ include 'autoloader.php';
           </div>
         </div>
       </div>
-      </div>
 
+      <div class="none infos" id="infos">
+            <div class="anuncio">
+              <div class="fotos">
+          <div class="carrocel" id="carrocel">
+                  <div id="carouselExampleIndicators" class="carousel slide">
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://marketplace.canva.com/wUgTo/MAGiKZwUgTo/1/tl/canva-avatar-icon-MAGiKZwUgTo.png" class=" img d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="   https://cdn-icons-png.flaticon.com/512/5064/5064052.png "img class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://lh3.googleusercontent.com/a/ACg8ocIcC9k0lsuZkDapdNakRqlss7SMkojqtLooZLGqpw2aGMrpKsI=s360-c-no" class="img d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+            <div class="btns">
+              <button id="btn1" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" onclick="btnUpdate(1)" class="btnC" aria-label="Slide 1"><img src="https://marketplace.canva.com/wUgTo/MAGiKZwUgTo/1/tl/canva-avatar-icon-MAGiKZwUgTo.png" class="btnLogo" alt="..."></button>
+              <button id="btn2" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" onclick="btnUpdate(2)" class="btnC" aria-label="Slide 2"><img src="   https://cdn-icons-png.flaticon.com/512/5064/5064052.png " class="btnLogo" alt="..."></button>
+              <button id="btn3" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" onclick="btnUpdate(3)" class="btnC" aria-label="Slide 3"><img src="https://lh3.googleusercontent.com/a/ACg8ocIcC9k0lsuZkDapdNakRqlss7SMkojqtLooZLGqpw2aGMrpKsI=s360-c-no" class="btnLogo" alt="..."></button>
+            </div>
+          </div>
+              </div>
+              <div class="descricao">
+                <h5 class='card-title' id='anuncioName'></h5>
+                <h6 class='card-preco' id='anuncioValor'></h6>
+            </div>
+        </div>
+      </div>
     </main>
 
     <div data-bs-theme="dark" class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -137,13 +176,52 @@ include 'autoloader.php';
       function updatePost() {
                 const name = document.getElementById('inputNome').value || 'Nome do Produto';
                 const price = document.getElementById('inputValor').value || '0,00';
-                const cover = document.getElementById('inputCapa').value || 'https://raw.githubusercontent.com/noirresina73-boop/fts/refs/heads/main/FT_broche_craneo%20(1).jpeg';
-
                 document.getElementById('cardName').textContent = name;
                 document.getElementById('cardValor').textContent = 'R$ ' + price;
-                document.getElementById('cardCapa').src = cover;
-
+                document.getElementById('anuncioName').textContent = name;
+                document.getElementById('anuncioValor').textContent = 'R$ ' + price;
               }
+
+          function handleImageUpload(event) {
+            const file = event.target.files[0];
+            if (!file) return;
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              uploadedImageURL = e.target.result;
+              document.getElementById('cardCapa').src = uploadedImageURL;
+              document.getElementById('anuncioCapa').src = uploadedImageURL;
+              document.getElementById('post-illustration').innerHTML = `<img src="${uploadedImageURL}" class="post-uploaded-img" alt="produto">`;
+            };
+            reader.readAsDataURL(file);
+          }
+
+          function noneCardUpdate(){
+            const post = document.getElementById('post');
+            post.classList = 'none post';
+            const infos = document.getElementById('infos');
+            infos.classList = 'infos';
+            const postagem = document.getElementById('postagem');
+            postagem.classList = 'postagem';
+          }
+          function noneCarrocelUpdate(){
+            const post = document.getElementById('post');
+            post.classList = 'post';
+            const infos = document.getElementById('infos');
+            infos.classList = 'none infos';
+            const postagem = document.getElementById('postagem');
+            postagem.classList = 'postagem fixed';
+          }
+
+          function btnUpdate(btn){
+            var botao = document.getElementById('btn1');
+            botao.classList = 'btnC';
+            var botao = document.getElementById('btn2');
+            botao.classList = 'btnC';
+            var botao = document.getElementById('btn3');
+            botao.classList = 'btnC';
+            var botao = document.getElementById('btn'+btn);
+            botao.classList = 'btnC activeBtn';
+          }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
