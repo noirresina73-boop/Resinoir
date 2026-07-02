4fb897720ea1988ef07022d28b234e0617e83fe2
decolevandro78-art <noirresina73@gmail.com>
2026-07-01 21:32:22 -0300

    n sei o q mudei

--- Gerenciar/Infos.php
+++ Gerenciar/Infos.php
@@ -89,19 +89,18 @@ include 'autoloader.php';
       </form>
       </div>
 
-      <div class="canva">
-        
+      <div class="postagem">
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
           <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
           <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
-
+          
           <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
           <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
         </div>
 
 
 
-      
+        <div class="post">
         <div class="card" style="width: 18rem;" data-bs-theme="dark">
           <img id="cardCapa" src="" class="card-img-top" alt="...">
           <div class="card-body">
@@ -109,7 +108,7 @@ include 'autoloader.php';
             <h6 class='card-preco' id='cardValor'></h6>
           </div>
         </div>
-      
+      </div>
       </div>
 
     </main>
